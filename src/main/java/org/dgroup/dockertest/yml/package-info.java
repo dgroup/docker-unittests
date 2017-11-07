09a822461c32e96992a17b2a6a32f00be567604c
dubinka <dubinka@netcracker.com>
2017-11-07 16:51:12 +0530

    0pd testing

--- src/main/java/org/dgroup/dockertest/yml/package-info.java
+++ src/main/java/org/dgroup/dockertest/yml/package-info.java
@@ -4,7 +4,7 @@
  * The main problem is that there is no proper object-oriented parser for .yml files,
  *  thus there are a lot of places with explicit classes casting.
  *
- * TODO implement a proper .yml loading layer
+ * @todo implement a proper .yml loading layer
  *
  * @author Yurii Dubinka (yurii.dubinka@gmail.com)
  * @since 0.1.0
